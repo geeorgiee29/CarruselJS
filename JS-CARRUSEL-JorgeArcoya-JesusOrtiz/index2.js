fbf83d3af830f1a4ccf10ea9747610497120e2a6
jesusO <jesusdortiz18@gmail.com>
2024-04-08 10:10:17 +0200

    añadiendo el timing al carrusel

--- JS-CARRUSEL-JorgeArcoya-JesusOrtiz/index2.js
+++ JS-CARRUSEL-JorgeArcoya-JesusOrtiz/index2.js
@@ -20,3 +20,36 @@ function cambiarOpinion(direccion) {
     // Muestra la siguiente opinión
     opiniones[siguienteIndice % opiniones.length].style.display = "block";
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const themeToggle = document.querySelector(".botonTema");
+    const body = document.body;
+
+    themeToggle.addEventListener("click", function() {
+        body.classList.toggle("dark-theme");
+        themeToggle.innerHTML = body.classList.contains("dark-theme") ? '<img src="./imagenes/sol.jpg" alt="Sol">' : '<img src="./imagenes/luna.jpg" alt="Luna">';
+    });
+}); 
+
+var temporizadorCarrusel;
+function iniciarCarruselAutomatico() {
+    temporizadorCarrusel = setInterval(function() {
+        cambiarOpinion(1);
+    }, 3000); // Cambia de opinión cada 3 segundos
+}
+
+// Función para detener el carrusel automático
+function detenerCarrusel() {
+    clearInterval(temporizadorCarrusel);
+}
+
+// Función para reiniciar el carrusel automático después de un clic
+function reiniciarCarrusel() {
+    iniciarCarruselAutomatico();
+}
+
+// Iniciar el carrusel automáticamente al cargar la página
+window.onload = function() {
+    iniciarCarruselAutomatico();
+};
