4a0798dd04a4ffb871499935e7b1ac2c0c152081
geeorgiee29 <166218780+geeorgiee29@users.noreply.github.com>
2024-04-08 10:14:47 +0200

    Update index2.js

--- JS-CARRUSEL-JorgeArcoya-JesusOrtiz/index2.js
+++ JS-CARRUSEL-JorgeArcoya-JesusOrtiz/index2.js
@@ -1,6 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const themeToggle = document.querySelector(".botonTema");
+    const body = document.body;
+
+    themeToggle.addEventListener("click", function() {
+        body.classList.toggle("dark-theme");
+        themeToggle.innerHTML = body.classList.contains("dark-theme") ? '<img src="./imagenes/sol.jpg" alt="Sol">' : '<img src="./imagenes/luna.jpg" alt="Luna">';
+    });
+});   
+
+
+
 function cambiarOpinion(direccion) {
     // Obtiene todas las opiniones
-    var opiniones = document.getElementsByClassName("opinion");
+    var opiniones = document.getElementsByClassName("contenidoCarrusel");
     // Obtiene el índice de la opinión actual
     var indiceActual = 0;
     for (var i = 0; i < opiniones.length; i++) {
@@ -18,37 +30,8 @@ function cambiarOpinion(direccion) {
         siguienteIndice = opiniones.length - 1;
     }
     // Muestra la siguiente opinión
-    opiniones[siguienteIndice % opiniones.length].style.display = "block";
+    opiniones[siguienteIndice % opiniones.length].style.display = "flex";
 }
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const themeToggle = document.querySelector(".botonTema");
-    const body = document.body;
-
-    themeToggle.addEventListener("click", function() {
-        body.classList.toggle("dark-theme");
-        themeToggle.innerHTML = body.classList.contains("dark-theme") ? '<img src="./imagenes/sol.jpg" alt="Sol">' : '<img src="./imagenes/luna.jpg" alt="Luna">';
-    });
-}); 
-
-var temporizadorCarrusel;
-function iniciarCarruselAutomatico() {
-    temporizadorCarrusel = setInterval(function() {
-        cambiarOpinion(1);
-    }, 3000); // Cambia de opinión cada 3 segundos
-}
-
-// Función para detener el carrusel automático
-function detenerCarrusel() {
-    clearInterval(temporizadorCarrusel);
-}
-
-// Función para reiniciar el carrusel automático después de un clic
-function reiniciarCarrusel() {
-    iniciarCarruselAutomatico();
-}
-
 // Iniciar el carrusel automáticamente al cargar la página
 window.onload = function() {
     iniciarCarruselAutomatico();
